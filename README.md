6d0f6ae2c81bd4db226a8b8063e3f951f10d1fd4
clayn <2429335889@qq.com>
2024-07-16 01:03:14 +0800

    fix: 修正cjs和esm导出的文件

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 
 ## 注意事项
 1. 是该模板创建项目后，将`.vitepress/config.mts`中的`base`改为项目名称 
+
+## 参考链接
+
+[发布一个 ESM 和 CJS 并存的 package](https://juejin.cn/post/7037386586899611684#heading-16)
+[unplugin-auto-import](https://github.com/unplugin/unplugin-auto-import/tree/main)
--- tsup.config.ts
+++ tsup.config.ts
@@ -4,11 +4,16 @@ export default defineConfig((options) => {
   return {
     entry: ['src/index.ts'],
     outDir: './lib',
-    format: ['cjs', 'esm', 'iife'],
+    format: ['cjs', 'esm'],
     dts: true,
     clean: true,
-    sourcemap: true,
+    sourcemap: !!options.watch,
     minify: !options.watch,
-    legacyOutput: true,
+    splitting: true,
+    cjsInterop: true,
+    esbuildPlugins: [
+      // 如果使用 import 语句引入了cjs模块，同时cjs模块导出的是module.exports.default, 那么可以通过如下方式纠正导出解决
+      // https://github.com/evanw/esbuild/issues/532#issuecomment-1028893869
+    ],
   }
 })
